02e04c5258682a5e2e07a75e0aae7d2b465ed4e2
zhengmingze <928205089@qq.com>
2016-03-16 11:39:01 +0800

    thrid change

--- fuli.java
+++ fuli.java
@@ -7,7 +7,7 @@ public class fuli {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		System.out.print("请选择单利1,复利2,本金3");
+		System.out.print("请选择单利1,复利2,本金3,时间4,利率5");
 		Scanner scanner=new Scanner(System.in);
 		int a=scanner.nextInt();
 		switch(a)
@@ -18,6 +18,12 @@ public class fuli {
 		break;
 		case 3:benjin();
 		break;
+		case 4:shijian();
+		break;
+		case 5:lilv();
+		break;
+		case 6:touzi();
+		break;
 		default:System.out.print("请重新输入");
 				main(null);
 				
@@ -26,7 +32,28 @@ public class fuli {
 
 }
 
- static void benjin() {
+ static void touzi() {
+		// TODO Auto-generated method stub
+	 Scanner scanner=new Scanner(System.in);
+		System.out.print("请输入本金");
+		double A=scanner.nextDouble();
+		System.out.print("请输入年利率");
+		double B=scanner.nextDouble();
+		System.out.print("请输入存入年限");
+		int C=scanner.nextInt();
+		double D=1;
+		double E=0;
+		for(int i=1;i<=C;i++)
+		{
+			D=D*(1+B);
+			E=E+D;
+			
+		}
+		System.out.println("复利终值"+String.format("%.4f", A*E+A));
+		main(null);
+	}
+
+static void benjin() {
 		// TODO Auto-generated method stub
 	 Scanner scanner=new Scanner(System.in);
 		System.out.print("请输入预期钱数");
@@ -76,4 +103,44 @@ static void danli()
 	System.out.println("单利终值"+(A+D));
 	main(null);
 }
+static void shijian()
+{
+	Scanner scanner=new Scanner(System.in);
+	System.out.print("请输入本金");
+	double A=scanner.nextDouble();
+	System.out.print("请输入年利率");
+	double B=scanner.nextDouble();
+	System.out.print("请输入存入预期金额");
+	double C=scanner.nextDouble();
+	double D=1;
+	double E=1;
+	int i=1;
+	
+	while(true)
+	{
+		D=D*(1+B);
+		E=A*D;
+		if(E<=C)
+		i++;
+		else break;
+	}
+	
+	System.out.println("需"+i+"年");
+	main(null);
 }
+static void lilv()
+{
+	Scanner scanner=new Scanner(System.in);
+	System.out.print("请输入本金");
+	double A=scanner.nextDouble();
+	System.out.print("请输入预期金额");
+	double B=scanner.nextDouble();
+	System.out.print("请输入存入年限");
+	double C=scanner.nextDouble();
+	double D=1;
+		D=Math.pow(B/A, 1/C)-1;
+System.out.println("利率为"+String.format("%.4f",D));
+	main(null);
+}
+}
+
